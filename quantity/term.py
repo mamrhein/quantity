4cd6daa71f4eac5d40b2f7882fccbe4baea7b32f
Michael Amrhein <michael@adrhinum.de>
2014-12-01 22:19:53 +0100

    Changed license.

--- quantity/term.py
+++ quantity/term.py
@@ -5,21 +5,11 @@
 ##
 ## Author:      Michael Amrhein (mamrhein@users.sourceforge.net)
 ##
-## Copyright:   (c) 2013 ff. Michael Amrhein
-## License:     This program is free software; you can redistribute it and/or
-##              modify it under the terms of the GNU Lesser General Public
-##              License as published by the Free Software Foundation; either
-##              version 2 of the License, or (at your option) any later
-##              version.
-##              This program is distributed in the hope that it will be
-##              useful, but WITHOUT ANY WARRANTY; without even the implied
-##              warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-##              PURPOSE.
-##              See the GNU Lesser General Public License for more details.
-##              You should have received a copy of the license text along with
-##              this program; if not, get it from http://www.gnu.org/licenses,
-##              or write to the Free Software Foundation, Inc.,
-##              59 Temple Place, Suite 330, Boston MA 02111-1307, USA
+## Copyright:   (c) 2012 ff. Michael Amrhein
+## License:     This program is free software. You can redistribute it, use it
+##              and/or modify it under the terms of the 2-clause BSD license.
+##              For license details please read the file LICENSE.TXT provided
+##              together with the source code.
 ##----------------------------------------------------------------------------
 ## $Source$
 ## $Revision$
