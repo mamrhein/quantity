8ed2351b336aa33785775c7cedbcfa13a2c5523d
Michael Amrhein <michael@adrhinum.de>
2015-06-13 18:00:18 +0200

    Modified setup.py and MANIFEST.in

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 with open('README.txt') as file:
@@ -11,12 +11,12 @@ setup(
     use_vcs_version=True,
     setup_requires=["hgtools"],
     install_requires=["decimalfp>=0.9.11"],
+    packages=find_packages(),
     author="Michael Amrhein",
     author_email="michael@adrhinum.de",
     url="https://pypi.python.org/pypi/quantity",
     description="Unit-safe computations with quantities (including money)",
     long_description=long_description,
-    packages=['quantity', 'quantity.money'],
     license='BSD',
     keywords='quantity quantities unit units money currency exchange',
     platforms='all',
