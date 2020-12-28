3b78df854d6a7af972db718869edb948aaf061eb
Michael Amrhein <michael@adrhinum.de>
2020-12-28 17:05:31 +0100

    Refactored registry (defined Protocol for items which can be registered).

--- src/quantity/qtyreg.py
+++ src/quantity/qtyreg.py
@@ -14,123 +14,109 @@
 """Global registry of defined quantities."""
 
 
-# TODO: uncomment the following when compatibility for Python 3.6 is dropped
-# from __future__ import annotations
-
 # Standard library imports
+from abc import abstractmethod
 from functools import partial
-from typing import Iterator, List, MutableMapping, Optional, TYPE_CHECKING
+from typing import Iterator, List, MutableMapping, Optional
+try:
+    from typing import Protocol
+except ImportError:
+    # noinspection PyUnresolvedReferences
+    from typing_extensions import Protocol
 
 # Third-party imports
 
 # Local imports
 from .term import Term
 
-if TYPE_CHECKING:
-    from . import Unit
-    from .qtymeta import QuantityMeta
+
+class SupportsDefinition(Protocol):
+
+    """Abstract base class for items holding a term as definition."""
+
+    @property
+    @abstractmethod
+    def definition(self) -> Term:
+        """Definition of `self`."""
+
+    @property
+    @abstractmethod
+    def normalized_definition(self) -> Term:
+        """Normalized definition of `self`.
+
+        Must be the normalized form of `self.definition`."""
 
 
-class QuantityClassRegistry:
+class DefinedItemRegistry:
 
-    """Registers Quantity classes by definition."""
+    """Registers items by definition."""
 
     def __init__(self) -> None:
-        self._qty_def_map: MutableMapping[Term, int] = {}
-        self._qty_cls_list: List[QuantityMeta] = []
+        self._item_def_map: MutableMapping[Term, int] = {}
+        self._item_list: List[SupportsDefinition] = []
 
-    def register_quantity_cls(self, qty_cls: 'QuantityMeta') -> int:
-        """Register Quantity class.
-
-        Registers a sub-class of :class:`Quantity` by its normalized
-        definition.
+    def register_item(self, item: SupportsDefinition) -> int:
+        """Register `item` by its normalized definition.
 
         Args:
-            qty_cls (QuantityMeta): sub-class of :class:`Quantity` to be
-                registered
+            item (SupportsDefinition): defined item to be registered
 
         Returns:
-            int: index of registered class
+            int: index of registered item
 
         Raises:
-            ValueError: class with same definition already registered
+            ValueError: item with same or equivalent definition already
+                registered
         """
-        qty_def = qty_cls.normalized_definition
+        item_norm_def = item.normalized_definition
         try:
-            idx = self._qty_def_map[qty_def]
+            idx = self._item_def_map[item_norm_def]
         except KeyError:
-            qty_cls_list = self._qty_cls_list
-            qty_cls_list.append(qty_cls)
-            idx = len(qty_cls_list) - 1
-            self._qty_def_map[qty_def] = idx
+            item_list = self._item_list
+            item_list.append(item)
+            idx = len(item_list) - 1
+            self._item_def_map[item_norm_def] = idx
             return idx
         else:
-            reg_cls = self._qty_cls_list[idx]
-            if reg_cls == qty_cls:
+            reg_item = self._item_list[idx]
+            if reg_item == item:
                 return idx
             else:
-                raise ValueError(
-                    "Class with same definition already registered.")
+                raise ValueError("Item with same or equivalent definition "
+                                 f"already registered: '{reg_item}'.")
 
-    def get_quantity_cls(self, qty_def: Term) -> 'QuantityMeta':
-        """Get Quantity class by definition.
+    def __getitem__(self, item_def: Term) -> SupportsDefinition:
+        """Get item by definition.
 
         Args:
-            qty_def (Term): definition of class to be looked-up
+            item_def (Term): definition of item to be looked-up
 
         Returns:
-            QuantityMeta: sub-class of :class:`Quantity` registered with
-                definition `qty_def`
+            SupportsDefinition: item registered with a definition equivalent
+                to `item_def`
 
         Raises:
-            ValueError: no sub-class of :class:`Quantity` registered with
-                definition `qtyDef`
+            ValueError: no item registered with definition equivalent to
+                `item_def`
         """
-        norm_qty_def = qty_def.normalized()
+        norm_qty_def = item_def.normalized()
         try:
-            idx = self._qty_def_map[norm_qty_def]
+            idx = self._item_def_map[norm_qty_def]
         except KeyError:
-            raise ValueError('No quantity class registered with given '
-                             'definition.')
-        return self._qty_cls_list[idx]
-
-    def get_unit_by_symbol(self, symbol: str) -> Optional['Unit']:
-        """Return the unit with symbol `symbol`.
-
-        Args:
-            symbol (str): symbol to look-up
-
-        Returns:
-            :class:`Unit` sub-class if a unit with given `symbol` exists in
-                one of the registered quantities' `Unit` class, otherwise
-                `None`
-        """
-        for qty_cls in self:
-            # noinspection PyUnresolvedReferences
-            unit = qty_cls.get_unit_by_symbol(symbol)
-            if unit:
-                return unit
-        return None
+            raise ValueError('No item registered with given definition.')
+        return self._item_list[idx]
 
     def __len__(self) -> int:
-        return len(self._qty_cls_list)
+        return len(self._item_list)
 
-    def __iter__(self) -> Iterator['QuantityMeta']:
-        return iter(self._qty_cls_list)
+    def __iter__(self) -> Iterator[SupportsDefinition]:
+        return iter(self._item_list)
 
 
 # Global registry of Quantities
-_registry = QuantityClassRegistry()
+_registry = DefinedItemRegistry()
 
 
-get_unit_by_symbol = partial(QuantityClassRegistry.get_unit_by_symbol,
-                             _registry)
-get_unit_by_symbol.__doc__ = _registry.get_unit_by_symbol.__doc__
-
-register_quantity_cls = partial(QuantityClassRegistry.register_quantity_cls,
+register_quantity_cls = partial(DefinedItemRegistry.register_item,
                                 _registry)
-register_quantity_cls.__doc__ = _registry.register_quantity_cls.__doc__
-
-get_quantity_cls = partial(QuantityClassRegistry.get_quantity_cls,
-                           _registry)
-get_quantity_cls.__doc__ = _registry.get_quantity_cls.__doc__
+register_quantity_cls.__doc__ = _registry.register_item.__doc__
