d53c0a24ba20c007d91523e9783f644c8cd7cc7f
Michael Amrhein <michael@adrhinum.de>
2021-04-25 17:36:16 +0200

    Adapted to new API; added __all__; updated doc.

--- src/quantity/predefined.py
+++ src/quantity/predefined.py
@@ -15,29 +15,6 @@
 # @formatter:off
 r"""Definitions of commonly used quantities and units.
 
-Length
-^^^^^^
-
-Reference unit: Metre ('m')
-
-Predefined units:
-
-====== ========================= ==================== ====================
-Symbol Name                      Definition           Equivalent in 'm'
-====== ========================= ==================== ====================
-µm     Micrometre                0.000001·m           0.000001
-mm     Millimetre                0.001·m              0.001
-cm     Centimetre                0.01·m               0.01
-in     Inch                      2.54·cm              0.0254
-dm     Decimetre                 0.1·m                0.1
-ft     Foot                      12·in                0.3048
-yd     Yard                      3·ft                 0.9144
-ch     Chain                     22·yd                20.1168
-fur    Furlog                    10·ch                201.1680
-km     Kilometre                 1000·m               1000
-mi     Mile                      8·fur                1609.3440
-====== ========================= ==================== ====================
-
 Mass
 ^^^^
 
@@ -48,13 +25,37 @@ Predefined units:
 ====== ========================= ==================== ====================
 Symbol Name                      Definition           Equivalent in 'kg'
 ====== ========================= ==================== ====================
-mg     Milligram                 0.000001·kg          0.000001
+mg     Milligram                 0.001·g              0.000001
 ct     Carat                     0.2·g                0.0002
 g      Gram                      0.001·kg             0.001
 oz     Ounce                     0.0625·lb            0.028349523125
 lb     Pound                     0.45359237·kg        0.45359237
 st     Stone                     14·lb                6.35029318
 t      Tonne                     1000·kg              1000
+====== ========================= ==================== ====================
+
+Length
+^^^^^^
+
+Reference unit: Metre ('m')
+
+Predefined units:
+
+====== ========================= ==================== ====================
+Symbol Name                      Definition           Equivalent in 'm'
+====== ========================= ==================== ====================
+nm     Nanometre                 0.000000001·m        0.000000001
+µm     Micrometre                0.000001·m           0.000001
+mm     Millimetre                0.001·m              0.001
+cm     Centimetre                0.01·m               0.01
+in     Inch                      2.54·cm              0.0254
+dm     Decimetre                 0.1·m                0.1
+ft     Foot                      12·in                0.3048
+yd     Yard                      3·ft                 0.9144
+ch     Chain                     22·yd                20.1168
+fur    Furlog                    10·ch                201.168
+km     Kilometre                 1000·m               1000
+mi     Mile                      8·fur                1609.344
 ====== ========================= ==================== ====================
 
 Duration
@@ -67,6 +68,9 @@ Predefined units:
 ====== ========================= ==================== ====================
 Symbol Name                      Definition           Equivalent in 's'
 ====== ========================= ==================== ====================
+ns     Nanosecond                0.000000001·s        0.000000001
+µs     Microsecond               0.000001·s           0.000001
+ms     Millisecond               0.001·s              0.001
 min    Minute                    60·s                 60
 h      Hour                      60·min               3600
 d      Day                       24·h                 86400
@@ -91,10 +95,10 @@ dm²    Square Decimetre          dm²                  0.01
 ft²    Square Foot               ft²                  0.09290304
 yd²    Square Yard               yd²                  0.83612736
 a      Are                       100·m²               100
-ac     Acre                      4840·yd²             4046.85642240
+ac     Acre                      4840·yd²             4046.8564224
 ha     Hectare                   100·a                10000
 km²    Square Kilometre          km²                  1000000
-mi²    Square Mile               mi²                  2589988.11033600
+mi²    Square Mile               mi²                  2589988.110336
 ====== ========================= ==================== ====================
 
 Volume
@@ -110,13 +114,13 @@ Predefined units:
 Symbol Name                      Definition           Equivalent in 'm³'
 ====== ========================= ==================== ====================
 mm³    Cubic Millimetre          mm³                  0.000000001
-ml     Millilitre                0.001·l              0.000001
 cm³    Cubic Centimetre          cm³                  0.000001
+ml     Millilitre                0.001·l              0.000001
 cl     Centilitre                0.01·l               0.00001
 in³    Cubic Inch                in³                  0.000016387064
 dl     Decilitre                 0.1·l                0.0001
-l      Litre                     0.001·m³             0.001
 dm³    Cubic Decimetre           dm³                  0.001
+l      Litre                     0.001·m³             0.001
 ft³    Cubic Foot                ft³                  0.028316846592
 yd³    Cubic Yard                yd³                  0.764554857984
 km³    Cubic Kilometre           km³                  1000000000
@@ -151,7 +155,7 @@ Predefined units:
 ====== ========================= ==================== ====================
 Symbol Name                      Definition           Equivalent in 'm/s²'
 ====== ========================= ==================== ====================
-mps²   Mile per Second squared   mi/s²                1609.3440
+mps²   Mile per Second squared   mi/s²                1609.344
 ====== ========================= ==================== ====================
 
 Force
@@ -159,7 +163,7 @@ Force
 
 Definition: Mass·Acceleration
 
-Reference unit: Newton ('N')
+Reference unit: Newton ('N' = 'kg·m/s²')
 
 Predefined units:
 
@@ -172,17 +176,18 @@ J/m    Joule per Metre           J/m                  1
 Energy
 ^^^^^^
 
-Definition: Length·Force
+Definition: Force·Length
 
-Reference unit: Joule ('J')
+Reference unit: Joule ('J' = 'N·m' = 'kg·m²/s²')
 
 Predefined units:
 
 ====== ========================= ==================== ====================
 Symbol Name                      Definition           Equivalent in 'J'
 ====== ========================= ==================== ====================
-Ws     Watt Second               s·W                  1
-kWh    Kilowatt Hour             h·kW                 3600000
+Nm     Newton Meter              N·m                  1
+Ws     Watt Second               W·s                  1
+kWh    Kilowatt Hour             kW·h                 3600000
 ====== ========================= ==================== ====================
 
 Power
@@ -190,7 +195,7 @@ Power
 
 Definition: Energy/Duration
 
-Reference unit: Watt ('W')
+Reference unit: Watt ('W' = 'J/s' = 'kg·m²/s³')
 
 Predefined units:
 
@@ -204,6 +209,23 @@ GW     Gigawatt                  1000000000·W         1000000000
 TW     Terawatt                  1000000000000·W      1000000000000
 ====== ========================= ==================== ====================
 
+Frequency
+^^^^^^^^^
+
+Definition: 1/Duration
+
+Reference unit: Hertz ('Hz' = '1/s')
+
+Predefined units:
+
+====== ========================= ==================== ====================
+Symbol Name                      Definition           Equivalent in 'Hz'
+====== ========================= ==================== ====================
+kHz    Kilohertz                 1000·Hz              1000
+MHz    Megahertz                 1000000·Hz           1000000
+GHz    Gigahertz                 1000000000·Hz        1000000000
+====== ========================= ==================== ====================
+
 DataVolume
 ^^^^^^^^^^
 
@@ -214,7 +236,7 @@ Predefined units:
 ====== ========================= ==================== ====================
 Symbol Name                      Definition           Equivalent in 'B'
 ====== ========================= ==================== ====================
-b      Bit                       1·B/8                0.125
+b      Bit                       0.125·B              0.125
 kb     Kilobit                   1000·b               125
 Kib    Kibibit                   1024·b               128
 kB     Kilobyte                  1000·B               1000
@@ -297,6 +319,7 @@ from . import Quantity, TableConverter
 from .si_prefixes import (
     CENTI, DECI, GIGA, KILO, MEGA, MICRO, MILLI, NANO, TERA,
     )
+from .term import Term
 
 
 class Mass(Quantity, ref_unit_name='Kilogram', ref_unit_symbol='kg'):
@@ -308,7 +331,7 @@ KILOGRAM = Mass.ref_unit
 
 GRAM = Mass.new_unit('g', 'Gram', MILLI * KILOGRAM)
 MILLIGRAM = Mass.new_unit('mg', 'Milligram', MILLI * GRAM)
-TONNE = Mass.new_unit('t', 'Tonne', MEGA * GRAM)
+TONNE = Mass.new_unit('t', 'Tonne', KILO * KILOGRAM)
 
 # some imperial units
 POUND = Mass.new_unit('lb', 'Pound', Decimal('0.45359237') * KILOGRAM)
@@ -370,18 +393,22 @@ class Area(Quantity,
 assert Area.ref_unit is not None
 SQUARE_METRE = Area.ref_unit
 
-SQUARE_MILLIMETRE = Area.new_unit('mm²', 'Square Millimetre', MILLIMETRE ** 2)
-SQUARE_CENTIMETRE = Area.new_unit('cm²', 'Square Centimetre', CENTIMETRE ** 2)
-SQUARE_DECIMETRE = Area.new_unit('dm²', 'Square Decimetre', DECIMETRE ** 2)
-SQUARE_KILOMETRE = Area.new_unit('km²', 'Square Kilometre', KILOMETRE ** 2)
+SQUARE_MILLIMETRE = Area.new_unit('mm²', 'Square Millimetre',
+                                  derive_from=MILLIMETRE)
+SQUARE_CENTIMETRE = Area.new_unit('cm²', 'Square Centimetre',
+                                  derive_from=CENTIMETRE)
+SQUARE_DECIMETRE = Area.new_unit('dm²', 'Square Decimetre',
+                                 derive_from=DECIMETRE)
+SQUARE_KILOMETRE = Area.new_unit('km²', 'Square Kilometre',
+                                 derive_from=KILOMETRE)
 ARE = Area.new_unit('a', 'Are', Decimal(100) * SQUARE_METRE)
 HECTARE = Area.new_unit('ha', 'Hectare', Decimal(100) * ARE)
 
 # some imperial units
-SQUARE_INCH = Area.new_unit('in²', 'Square Inch', INCH ** 2)
-SQUARE_FOOT = Area.new_unit('ft²', 'Square Foot', FOOT ** 2)
-SQUARE_YARD = Area.new_unit('yd²', 'Square Yard', YARD ** 2)
-SQUARE_MILE = Area.new_unit('mi²', 'Square Mile', MILE ** 2)
+SQUARE_INCH = Area.new_unit('in²', 'Square Inch', derive_from=INCH)
+SQUARE_FOOT = Area.new_unit('ft²', 'Square Foot', derive_from=FOOT)
+SQUARE_YARD = Area.new_unit('yd²', 'Square Yard', derive_from=YARD)
+SQUARE_MILE = Area.new_unit('mi²', 'Square Mile', derive_from=MILE)
 ACRE = Area.new_unit('ac', 'Acre', Decimal(4840) * SQUARE_YARD)
 
 
@@ -394,10 +421,14 @@ class Volume(Quantity,
 assert Volume.ref_unit is not None
 CUBIC_METRE = Volume.ref_unit
 
-CUBIC_MILLIMETRE = Volume.new_unit('mm³', 'Cubic Millimetre', MILLIMETRE ** 3)
-CUBIC_CENTIMETRE = Volume.new_unit('cm³', 'Cubic Centimetre', CENTIMETRE ** 3)
-CUBIC_DECIMETRE = Volume.new_unit('dm³', 'Cubic Decimetre', DECIMETRE ** 3)
-CUBIC_KILOMETRE = Volume.new_unit('km³', 'Cubic Kilometre', KILOMETRE ** 3)
+CUBIC_MILLIMETRE = Volume.new_unit('mm³', 'Cubic Millimetre',
+                                   derive_from=MILLIMETRE)
+CUBIC_CENTIMETRE = Volume.new_unit('cm³', 'Cubic Centimetre',
+                                   derive_from=CENTIMETRE)
+CUBIC_DECIMETRE = Volume.new_unit('dm³', 'Cubic Decimetre',
+                                  derive_from=DECIMETRE)
+CUBIC_KILOMETRE = Volume.new_unit('km³', 'Cubic Kilometre',
+                                  derive_from=KILOMETRE)
 
 # litre
 LITRE = Volume.new_unit('l', 'Litre', MILLI * CUBIC_METRE)
@@ -406,9 +437,9 @@ CENTILITRE = Volume.new_unit('cl', 'Centilitre', CENTI * LITRE)
 DECILITRE = Volume.new_unit('dl', 'Decilitre', DECI * LITRE)
 
 # some imperial units
-CUBIC_INCH = Volume.new_unit('in³', 'Cubic Inch', INCH ** 3)
-CUBIC_FOOT = Volume.new_unit('ft³', 'Cubic Foot', FOOT ** 3)
-CUBIC_YARD = Volume.new_unit('yd³', 'Cubic Yard', YARD ** 3)
+CUBIC_INCH = Volume.new_unit('in³', 'Cubic Inch', derive_from=INCH)
+CUBIC_FOOT = Volume.new_unit('ft³', 'Cubic Foot', derive_from=FOOT)
+CUBIC_YARD = Volume.new_unit('yd³', 'Cubic Yard', derive_from=YARD)
 
 
 class Velocity(Quantity,
@@ -420,19 +451,19 @@ class Velocity(Quantity,
 assert Velocity.ref_unit is not None
 METRE_PER_SECOND = Velocity.ref_unit
 KILOMETRE_PER_HOUR = Velocity.new_unit('km/h', 'Kilometre per hour',
-                                       KILOMETRE / HOUR)  # type: ignore
+                                       derive_from=(KILOMETRE, HOUR))
 
 # some imperial units
 FOOT_PER_SECOND = Velocity.new_unit('ft/s', 'Foot per Second',
-                                    FOOT / SECOND)  # type: ignore
+                                    derive_from=(FOOT, SECOND))
 MILE_PER_HOUR = Velocity.new_unit('mph', 'Mile per Hour',
-                                  MILE / HOUR)  # type: ignore
+                                  derive_from=(MILE, HOUR))
 
 
 class Acceleration(Quantity,
-                   define_as=Velocity / Duration,
+                   define_as=Length / Duration ** 2,
                    ref_unit_name='Metre per Second squared'):
-    """Acceleration = Velocity / Duration"""
+    """Acceleration = Length / Duration²"""
 
 
 assert Acceleration.ref_unit is not None
@@ -441,7 +472,7 @@ METRE_PER_SECOND_SQUARED = Acceleration.ref_unit
 # some imperial units
 MILE_PER_SECOND_SQUARED = Acceleration.new_unit(
     'mps²', 'Mile per Second squared',
-    (MILE / SECOND) / SECOND)  # type: ignore
+    derive_from=(MILE, SECOND))
 
 
 class Force(Quantity,
@@ -464,6 +495,9 @@ class Energy(Quantity,
 
 assert Energy.ref_unit is not None
 JOULE = Energy.ref_unit
+
+NEWTON_METER = Energy.new_unit('Nm', 'Newton Meter',
+                               derive_from=(NEWTON, METRE))
 
 
 class Power(Quantity,
@@ -500,14 +534,18 @@ GIGAHERTZ = Frequency.new_unit('GHz', 'Gigahertz', GIGA * HERTZ)
 # some more unit definitions based on others than the reference units
 
 # Force
+# noinspection PyTypeChecker
 JOULE_PER_METRE = Force.new_unit('J/m', 'Joule per Metre',
-                                 JOULE / METRE)  # type: ignore
+                                 Term(((JOULE, 1), (METRE, -1))))
 
 # Energy
+# noinspection PyTypeChecker
 WATT_SECOND = Energy.new_unit('Ws', 'Watt Second',
-                              WATT * SECOND)  # type: ignore
+                              Term(((WATT, 1), (SECOND, 1))))
+# noinspection PyTypeChecker
+
 KILOWATT_HOUR = Energy.new_unit('kWh', 'Kilowatt Hour',
-                                KILOWATT * HOUR)  # type: ignore
+                                Term(((KILOWATT, 1), (HOUR, 1))))
 
 
 class DataVolume(Quantity,
@@ -567,7 +605,7 @@ TEBIBYTE_PER_SECOND = DataThroughput.new_unit(
     'TiB/s', 'Tebibyte per Second', Decimal(2) ** 40 * BYTE_PER_SECOND)
 
 BIT_PER_SECOND = DataThroughput.new_unit(
-    'b/s', 'Bit per Second', BIT / SECOND)  # type: ignore
+    'b/s', 'Bit per Second', derive_from=(BIT, SECOND))
 KILOBIT_PER_SECOND = DataThroughput.new_unit(
     'kb/s', 'Kilobit per Second', KILO * BIT_PER_SECOND)
 MEGABIT_PER_SECOND = DataThroughput.new_unit(
@@ -605,3 +643,133 @@ _temp_conv = [
     ]
 
 Temperature.register_converter(TableConverter(_temp_conv))
+
+__all__ = [
+    'ACRE',
+    'ARE',
+    'Acceleration',
+    'Area',
+    'BIT',
+    'BIT_PER_SECOND',
+    'BYTE',
+    'BYTE_PER_SECOND',
+    'CARAT',
+    'CELSIUS',
+    'CENTILITRE',
+    'CENTIMETRE',
+    'CHAIN',
+    'CUBIC_CENTIMETRE',
+    'CUBIC_DECIMETRE',
+    'CUBIC_FOOT',
+    'CUBIC_INCH',
+    'CUBIC_KILOMETRE',
+    'CUBIC_METRE',
+    'CUBIC_MILLIMETRE',
+    'CUBIC_YARD',
+    'DAY',
+    'DECILITRE',
+    'DECIMETRE',
+    'DataThroughput',
+    'DataVolume',
+    'Duration',
+    'Energy',
+    'FAHRENHEIT',
+    'FOOT',
+    'FOOT_PER_SECOND',
+    'FURLONG',
+    'Force',
+    'Frequency',
+    'GIBIBIT',
+    'GIBIBIT_PER_SECOND',
+    'GIBIBYTE',
+    'GIBIBYTE_PER_SECOND',
+    'GIGABIT',
+    'GIGABIT_PER_SECOND',
+    'GIGABYTE',
+    'GIGABYTE_PER_SECOND',
+    'GIGAHERTZ',
+    'GIGAWATT',
+    'GRAM',
+    'HECTARE',
+    'HERTZ',
+    'HOUR',
+    'INCH',
+    'JOULE',
+    'JOULE_PER_METRE',
+    'KELVIN',
+    'KIBIBIT',
+    'KIBIBIT_PER_SECOND',
+    'KIBIBYTE',
+    'KIBIBYTE_PER_SECOND',
+    'KILOBIT',
+    'KILOBIT_PER_SECOND',
+    'KILOBYTE',
+    'KILOBYTE_PER_SECOND',
+    'KILOGRAM',
+    'KILOHERTZ',
+    'KILOMETRE',
+    'KILOMETRE_PER_HOUR',
+    'KILOWATT',
+    'KILOWATT_HOUR',
+    'LITRE',
+    'Length',
+    'MEBIBIT',
+    'MEBIBIT_PER_SECOND',
+    'MEBIBYTE',
+    'MEBIBYTE_PER_SECOND',
+    'MEGABIT',
+    'MEGABIT_PER_SECOND',
+    'MEGABYTE',
+    'MEGABYTE_PER_SECOND',
+    'MEGAHERTZ',
+    'MEGAWATT',
+    'METRE',
+    'METRE_PER_SECOND',
+    'METRE_PER_SECOND_SQUARED',
+    'MICROMETRE',
+    'MICROSECOND',
+    'MILE',
+    'MILE_PER_HOUR',
+    'MILE_PER_SECOND_SQUARED',
+    'MILLIGRAM',
+    'MILLILITRE',
+    'MILLIMETRE',
+    'MILLISECOND',
+    'MILLIWATT',
+    'MINUTE',
+    'Mass',
+    'NANOMETRE',
+    'NANOSECOND',
+    'NEWTON',
+    'NEWTON_METER',
+    'OUNCE',
+    'POUND',
+    'Power',
+    'SECOND',
+    'SQUARE_CENTIMETRE',
+    'SQUARE_DECIMETRE',
+    'SQUARE_FOOT',
+    'SQUARE_INCH',
+    'SQUARE_KILOMETRE',
+    'SQUARE_METRE',
+    'SQUARE_MILE',
+    'SQUARE_MILLIMETRE',
+    'SQUARE_YARD',
+    'STONE',
+    'TEBIBIT',
+    'TEBIBIT_PER_SECOND',
+    'TEBIBYTE',
+    'TEBIBYTE_PER_SECOND',
+    'TERABIT',
+    'TERABIT_PER_SECOND',
+    'TERABYTE',
+    'TERABYTE_PER_SECOND',
+    'TERAWATT',
+    'TONNE',
+    'Temperature',
+    'Velocity',
+    'Volume',
+    'WATT',
+    'WATT_SECOND',
+    'YARD'
+    ]
